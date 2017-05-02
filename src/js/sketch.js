659098d38d81993dbab75fc70e1106d59cdb91e3
Daniel Dias de Lima <daniel.dddl.pdf@gmail.com>
2017-05-02 18:26:11 -0300

    Mudança de cor e acrescimo de funcionalidades visuais

--- src/js/sketch.js
+++ src/js/sketch.js
@@ -1,16 +1,19 @@
 var tabuleiro;
 
 function setup(){
+
   createCanvas(325,500);
   tabuleiro = new Tabuleiro(4,4);
 }
 
 function draw(){
-  background(100);
+  background(225);
+  textSize(25);
+  text("JOGO DA MEMÓRIA", 42,410);
   tabuleiro.show();
 }
 
 function mousePressed(){
   tabuleiro.cliqueMouse(mouseX, mouseY);
-  
+
 }
